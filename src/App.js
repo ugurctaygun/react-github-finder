569d51c0c230c92e8bc3b5bfb2cde5f138701512
unknown <ugurtaygun@hotmail.com>
2020-02-28 20:07:05 +0300

    Dynamic and Static proptypes
    Declaring props for dynamic data, usin g prop Types  to check and validate if input is matched with the type

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
-import React from 'react';
+import React, {Component} from 'react';
 import './App.css';
+import NavBar from './components/layout/NavBar'
 
-function App() {
-  return (
-    <div className="App">
-      Hieeello World
-    </div>
-  );
+class App extends Component {
+
+  render() {
+    const name = "john doe";
+    const loading = true;
+
+    return (
+      <div className="App">
+        <NavBar title=" Github Finder" logo="fab fa-github"/>
+        {loading ? <h3>Loading...</h3> : <h1>Welcome {name}</h1>}
+      </div>
+    );
+  }
+  
 }
 
 export default App;
